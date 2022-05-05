9a0c811589c8a7f3512e25872d69ba6c24cc93ef
C <38694948+CedricBeausseron@users.noreply.github.com>
2022-05-05 15:24:13 +0200

    commit

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,14 +3,18 @@ if(! window.location.href.includes("https://cedricbeausseron.github.io")) {
     rootPath = "nophp/cedricbeausseron.github.io/"
 }
 
+//fonctions de templates
 function experiences(){
     let progressBars = document.getElementsByClassName("progress-bar");
     for (let item of progressBars) {
-        console.log(item);
-        item.style.width = "70%";
+        // console.log(item.getAttribute("aria-valuenow"));
+        console.log(item.closest('.progress').style.visibility)
+        // item.closest('.progress').style.visibility = "visible;"
+        item.style.width = item.getAttribute("aria-valuenow")+"%";
     }
 }
 
+//appeler une fonction par son nom en string
 function executeFunctionByName(functionName, context /*, args */) {
     var args = Array.prototype.slice.call(arguments, 2);
     var namespaces = functionName.split(".");
