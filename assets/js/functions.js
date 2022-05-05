2bb72c45df4633cc557b4daa278a8bd7a6e47242
C <38694948+CedricBeausseron@users.noreply.github.com>
2022-05-05 14:49:33 +0200

    commit

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,6 +3,26 @@ if(! window.location.href.includes("https://cedricbeausseron.github.io")) {
     rootPath = "nophp/cedricbeausseron.github.io/"
 }
 
+function experiences(){
+    let progressBars = document.getElementsByClassName("progress-bar");
+    for (let item of progressBars) {
+        console.log(item);
+        item.style.width = "70%";
+    }
+}
+
+function executeFunctionByName(functionName, context /*, args */) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    var namespaces = functionName.split(".");
+    var func = namespaces.pop();
+    for(var i = 0; i < namespaces.length; i++) {
+    context = context[namespaces[i]];
+}
+    if(context[func]){
+        return context[func].apply(context, args);
+    }
+}
+
 function getHtml(htmlType, htmlName){
     fetch("/"+rootPath+htmlType+"/"+htmlName+".html")
     .then(response => {
@@ -10,12 +30,13 @@ function getHtml(htmlType, htmlName){
     })
     .then(data => {
         document.querySelector(htmlName).innerHTML = data;
+        
+        executeFunctionByName(htmlName, window, "truc")
     });
 }
 function getTemplate(htmlName){
     getHtml("template-parts", htmlName)
 }
-
 function getPage(htmlName){
     getHtml("pages", htmlName)
 }
